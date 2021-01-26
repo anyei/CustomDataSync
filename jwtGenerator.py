e28d60862bce9f2a682229278e8f2e88efb48373
Angel Robles <angel.robles@accuv.com>
2021-01-25 23:58:47 -0400

    add jwt generator

---
+++ jwtGenerator.py
@@ -0,0 +1,45 @@
+# pip install jwt cryptography requests
+
+from datetime import datetime
+import jwt
+import time
+import requests
+
+# *** Update these values to match your configuration ***
+IS_SANDBOX = False
+KEY_FILE = 'salesforce.key'
+ISSUER ='connected app client id'
+SUBJECT = 'youruser@email.com'
+EXPTIME = int(time.time()) + (60 * 10000)
+
+# *******************************************************
+
+DOMAIN = 'test' if IS_SANDBOX else 'login'
+
+print('Loading private key...')
+with open(KEY_FILE) as fd:
+    private_key = fd.read()
+
+print('Generating signed JWT assertion...')
+claim = {
+    'iss': ISSUER,
+    'exp': EXPTIME,
+    'aud': 'https://{}.salesforce.com'.format(DOMAIN),
+    'sub': SUBJECT,
+}
+assertion = jwt.encode(claim, private_key, algorithm='RS256', headers={'alg':'RS256'})#.decode('utf8')
+
+f = open("jwt.txt", "a")
+f.write(assertion)
+f.close()
+
+print('Assertion ', assertion)
+
+print('Making OAuth request...')
+r = requests.post('https://{}.salesforce.com/services/oauth2/token'.format(DOMAIN), data = {
+    'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+    'assertion': assertion
+})
+
+print('Status:', r.status_code)
+print(r.json())
